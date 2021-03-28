51d8b2454d1027bf63f70da0b625494dba1e09ad
Johan Ronsse <johan.ronsse@gmail.com>
2021-03-28 20:54:17 +0200

    Setup for vercel

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,7 @@
 const sveltePreprocess = require('svelte-preprocess');
 const node = require('@sveltejs/adapter-node');
 const pkg = require('./package.json');
+const vercel = require('@sveltejs/adapter-vercel');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
@@ -25,3 +26,4 @@ module.exports = {
 		}
 	}
 };
+
