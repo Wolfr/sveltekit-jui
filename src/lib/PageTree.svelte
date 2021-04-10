e316bf247a90aee863eb63f86061ba901a22be27
Johan Ronsse <johan.ronsse@gmail.com>
2021-04-10 15:41:55 +0200

    Fix bug

--- src/lib/PageTree.svelte
+++ src/lib/PageTree.svelte
@@ -13,10 +13,7 @@
 </script>
 
 
-<svelte:window
-  bind:innerWidth={windowInnerWidth}
-></svelte:window>
-{$pageIndexVisibility[0].pageIndexVisible}
+<svelte:window bind:innerWidth={windowInnerWidth} />
 <ul class="c-bordered-list c-bordered-list--small">
   <li>
     <a href="/styleguide/" on:click={toggleVisibilityIfMobile}>Style guide</a>
