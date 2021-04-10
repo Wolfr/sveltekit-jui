cad07fedd705e763fcea42221d8848f2cdc211aa
Johan Ronsse <johan.ronsse@gmail.com>
2021-04-10 20:36:32 +0200

    Adjust Icon to have support for SVG type

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,7 +1,18 @@
 <script>
-    export let icon;
-    export let iconAnimatedClass = null;
-    export let style = null;
+
+  import SvgIcon from '$lib/SvgIcon.svelte';
+
+  export let type = "icon-font";
+  export let icon;
+  export let iconAnimatedClass = null;
+  export let style = null;
+
 </script>
 
-<i class="c-icon mi mi-{icon} {iconAnimatedClass}" {style}></i>
+{#if type == "svg"}
+  <SvgIcon {icon} {iconAnimatedClass} />
+{/if}
+
+{#if type == "icon-font"}
+  <i class="c-icon mi mi-{icon} {iconAnimatedClass}" {style}></i>
+{/if}
