b60a48bbaff56f6dc5fb816cfddeabb234387048
example <example@example.com>
2021-04-11 12:06:57 -0500

    use exported icons

--- src/lib/SideMenuItem.svelte
+++ src/lib/SideMenuItem.svelte
@@ -9,7 +9,7 @@
 <li class="c-side-menu__item { $page.path == href ? 'c-side-menu__item--active' : ''}">
   <a href={href}>
     {#if icon}
-      <Icon icon="add" />
+      <Icon style="margin-right: .8rem;" bind:icon />
     {/if}
     <span><slot /></span>
   </a>
