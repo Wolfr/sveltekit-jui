a957f4f6d09867f6f7193153ef662bba3b6ef2d3
Johan Ronsse <johan.ronsse@gmail.com>
2021-04-13 19:41:56 +0200

    Add examples

--- src/routes/styleguide/components/pagination.svelte
+++ src/routes/styleguide/components/pagination.svelte
@@ -14,5 +14,41 @@
 <StyleguideIntro title={pageTitle} />
 
 <StyleguideCard title={pageTitle}>
-  <Pagination totalPages=15 />
+  <Pagination totalPages=1 currentPage=1 />
+  <br/>
+  <Pagination totalPages=2 currentPage=1 />
+  <br/>
+  <Pagination totalPages=3 currentPage=2 />
+  <br/>
+  <Pagination totalPages=4 currentPage=2 />
+  <br/>
+  <Pagination totalPages=5 currentPage=2 />
+  <br/>
+  <Pagination totalPages=6 currentPage=2 />
+  <br/>
+  <Pagination totalPages=7 currentPage=2 />
+  <br/>
+  <Pagination totalPages=8 currentPage=2 />
+  <br/>
+  <Pagination totalPages=9 pageLimit=7 currentPage=2 />
+  <br/>
+  
+  <p>Current page is 12, totalpages 20</p>
+  <br/>
+  <Pagination totalPages=20 currentPage=12 />
+  
+  <br/>
+  
+  <p>Current page is 12, totalpages 30</p>
+  <br/>
+  <Pagination totalPages=30 currentPage=12 />
+
+  <p>Current page is 1, totalpages 100</p>
+  <br/>
+  <Pagination totalPages=100 currentPage=1 />
+  <br/>
+
+  <p>Current page is 54, totalpages 70</p>
+  <!-- <Pagination totalPages=70 currentPage=54 /> -->
+
 </StyleguideCard>
