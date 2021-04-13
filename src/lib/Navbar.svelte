095464a17ed6b2a6dbd532d0e2c99866997d1b61
Johan Ronsse <johan.ronsse@gmail.com>
2021-04-13 21:17:21 +0200

    Change navbar API

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,15 +4,19 @@
     Navbar - Wrapper for navigation bars
     
     @export {enum} border - top, bottom
+    @export {enum} skin - alt
 
   */
   export let borderPosition = "bottom";
-  export let skin = "white";
+  export let skin = null;
 
 </script>
 
-{#if borderPosition == "top"}
-  <div class="c-navbar c-navbar--{skin} c-navbar--bordered-top"><slot /></div>
-{:else}
-  <div class="c-navbar c-navbar--{skin} c-navbar--bordered-bottom"><slot /></div>
-{/if}
+<div
+  class="c-navbar"
+  class:c-navbar--alt={skin=="alt"}
+  class:c-navbar--bordered-top={borderPosition=="top"}
+  class:c-navbar--bordered-bottom={borderPosition!="top"}
+>
+<slot />
+</div>
